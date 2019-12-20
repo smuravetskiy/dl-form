434e9dec04a159d58d156c39616f16869e3eb44f
smuravetskiy <smuravetskiy@lohika.com>
2019-12-20 12:21:24 +0200

    adds post handler

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
+const ics = require('ics');
+const uuid = require('uuid');
+const moment = require('moment');
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+const formatDate = ({ start }) => {
+  const m = new moment(start);
+  return [
+    m.year(),
+    m.month(),
+    m.date(),
+    m.hour(),
+    m.minute()
+  ];
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  evts = req.body;
+  const { error, value } = ics.createEvents(
+    evts.map(evt => ({
+      ...evt,
+      start: formatDate(evt),
+      duration: {
+        hours: 1
+      },
+      status: 'CONFIRMED',
+      busyStatus: 'BUSY',
+    }))
+  );
+
+  if (error) {
+    res
+      .status(400)
+      .send(error);
+  }
+
+  const fileName = uuid.v4();
+  writeFileSync(`${__dirname}/${fileName}.ics`, value);
+
+  res
+    .status(200)
+    .sendFile(fileName);
+});
 
 app.listen(
   process.env.PORT,
