8da417804bdfb456e1df7b3c438a934dcf34b789
smuravetskiy <smuravetskiy@lohika.com>
2019-12-20 15:29:26 +0200

    log

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
+  console.log('!!!', req.body)
+
   evts = JSON.parse(
     req.body
   );
