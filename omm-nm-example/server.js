11e9d2de0b3019edb18122272ee701001fdf61bb
timofeymelnik <timofeymelnik@gmail.com>
2020-03-02 12:26:48 +0200

    Add validator class

--- omm-nm-example/server.js
+++ omm-nm-example/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Validate = require('./utils/validate')
 
 const app = express();
 const port = 3000;
@@ -20,10 +21,21 @@ mongoose
 
 const Monument = require('./models/Monument');
 
+// render
 app.get('/', function (req, res) {
   res.render('index')
 });
 
+app.get('/update/:id',function (req, res) {
+  res.render('update', {id: req.params.id})
+});
+
+app.get('/create',function (req, res) {
+  res.render('create')
+});
+
+
+// API
 app.get('/api/monuments', function (req, res) {
   Monument.find()
     .then(items => res.json(items))
@@ -33,30 +45,58 @@ app.get('/api/monuments', function (req, res) {
     });
 });
 
-
 app.get('/api/monuments/:id', function (req, res) {
+  const id = req.params.id
+  
+  // Пример использования валидатора для проверки id на корректность
+  const validator = new Validate(id)
+
+  if (!validator
+      .isMongoID()
+      .isValid) {
+    // если id не корректный нам нужно вернуть ошибку
+    res.status(500).send('id should be mongoID!')
+    // и прервать выполнение функции
+    return
+  }
+
   Monument
-    .find({ _id: req.params.id })
-    .then(item => res.json(item))
+    .find({ _id: id })
+    .then(item => res.json(item[0]))
     .catch(err => {
       res.status(500).send('Something Wrong!')
       console.log(err)
     });
 });
 
+app.put('/api/monuments/:id', function (req, res) {
+  // нужно ли проверить id?
+  // нужно ли проверить есть ли name внутри req.body?
+  Monument 
+    .update({_id: req.params.id}, req.body)
+    .then((item) => res.json(item))
+    .catch(err => {
+      res.status(500).send(`Something Wrong!:${err}`)
+      console.log(err)
+    })
+});
+
 app.post('/api/monuments', (req, res) => {
+  // нужно ли проверить есть ли name в req.body?
+  // нужно ли проверить правильного ли формата дата которую мы передаем?
   const newMonument = new Monument(req.body);
 
   newMonument
     .save()
     .then((item) => res.json(item))
     .catch(err => {
-      res.status(500).send('Something Wrong!')
+      res.status(500).send(`Something Wrong! ${err}`)
       console.log(err)
     });
 });
 
 app.delete('/api/monuments/:id', function (req, res) {
+  // нужно ли проверить id?
   Monument
     .deleteOne({ _id: req.params.id })
     .then(() => res.send(200))
